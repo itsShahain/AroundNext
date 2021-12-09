ac3bc3458a1aa2cd4d976056670b94829ed37ca9
itsShahain <shahain.live@gmail.com>
2021-12-09 13:33:25 +0530

    Final Commit

--- Scholastic/application/views/SignIn.php
+++ Scholastic/application/views/SignIn.php
@@ -5,7 +5,6 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
   <script defer src="<?php echo base_url("application/assets/js/SignIn.js") ?>"></script>
   <script src="https://kit.fontawesome.com/ded7d2cbbf.js" crossorigin="anonymous"></script>
   <link rel="shortcut icon" href="<?php echo base_url("application/assets/images/book-white-title.png"); ?>" type="image/x-icon" />
